ce4af7db67ab858f41f62e2c55645c70f7e05ed3
artjvl <artvanliere@gmail.com>
2020-03-17 10:52:17 +0100

    Finished backward method (untested)

--- unet/sigurd_model.py
+++ unet/sigurd_model.py
@@ -3,6 +3,7 @@ import torch
 from .art_net import ArtNet
 from .pavel_net import PavelNet
 
+
 class SigurdModel(object):
     def __init__(self):
         self.nn = None
@@ -12,28 +13,40 @@ class SigurdModel(object):
         self.opt_discriminator = torch.optim.Adam(self.discriminator.parameters())
         self.opt_generator = torch.optim.Adam(self.generator.parameters())
 
-        self.real_A = None
-        self.real_B = None
-        self.fake_B = None
+        self.real_x = None
+        self.real_y = None
+        self.fake_y = None
 
-    def set_input(self, real_A, real_B):
-        self.real_A = real_A
-        self.real_B = real_B
+    def set_input(self, real_x, real_y):
+        self.real_x = real_x
+        self.real_y = real_y
 
     def forward(self):
-        self.fake_B = self.generator.forward(self.real_A)
+        self.fake_y = self.generator.forward(self.real_x)
 
     def backward(self):
-        # discriminator
-        fake_AB = torch.cat((self.real_A, self.fake_B), 1)
-        pred_fake = self.discriminator(fake_AB)
+        # generate "fake" image from real mask
+        fake_xy = torch.cat((self.real_x, self.fake_y), 1)
+
+        pred_fake = self.discriminator(fake_xy)
+        # for training the discriminator, the prediction of the fake image should be True
         loss_discriminator_fake = self.nn.MSELoss(pred_fake, False)
 
         self.forward()
-        real_AB = torch.cat((self.real_A, self.real_B), 1)
-        pred_real = self.discriminator(real_AB)
+        real_xy = torch.cat((self.real_x, self.real_y), 1)
+        pred_real = self.discriminator(real_xy)
+        # for training the discriminator, the prediction of the real image should be False
         loss_discriminator_true = self.nn.MSELoss(pred_real, True)
 
         loss_discriminator = 0.5 * (loss_discriminator_fake + loss_discriminator_true)
-        loss_discriminator.backward()
 
+        self.opt_discriminator.zero_grad()
+        loss_discriminator.backward()
+        self.opt_discriminator.step()
+
+        # for training the generator, the prediction of the fake image should be True
+        loss_generator = self.nn.MSELoss(pred_fake, True)
+
+        self.opt_generator.zero_grad()
+        loss_generator.backward()
+        self.opt_generator.step()
